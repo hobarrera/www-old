50ecba489443ea7f1049538c260980cdeb9f5b42
Hugo Osvaldo Barrera <hugo@barrera.io>
2012-07-17 01:04:12 -0300

    Updated update script

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ sync
 
 rsync -rtvzchlC --progress --stats --delete-after --rsync-path=/usr/local/bin/rsync  \
    /home/hugo/workspace/www/code/  \
-   root@elysion.ubertech.com.ar:/var/www/sites/default/  \
+   root@elysion.ubertech.com.ar:/var/www/https/ar.com.hugoosvaldobarrera/ \
    -e 'ssh -i/home/hugo/.certs/root@elysion.unencrypted'
 
 echo "Update complete"
