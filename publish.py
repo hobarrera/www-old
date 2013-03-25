9b4de64a9fe9a39a62f730049ad8cbaf9857ea5a
Hugo Osvaldo Barrera <hugo@barrera.io>
2013-03-25 15:38:41 -0300

    Upgraded publish.py:
     - File pre-processing is done.
     - Publish files through rsync.
    There's still lots of WIP.

--- publish.py
+++ publish.py
@@ -1,15 +1,75 @@
 #!/usr/bin/env python2
 
 import os
+import sys
+import shutil
+import json
+from docopt import docopt
 
-local_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"source/")
-remote_path = "/var/www/https/ar.com.hugoosvaldobarrera/"
+usage = """Publish.py
 
-rsync_command = """\
-rsync -rtvzchlC --progress --stats --rsync-path=/usr/local/bin/rsync  \
-   {} root@elysion.ubertech.com.ar:{}  \
-""".format(local_path, remote_path)
-# use --delete-after to delete files that don't exist locally
+Usage:
+  publish.py publish
+  publish.py build
+  publish.py -h | --help
+  publish.py --version
 
-# TODO: rebuild CV from tex (submodule) and push that. gitignore the pdf
-os.system(rsync_command)
+"""
+
+class SettingsLoader:
+
+    def load(filename="settings.json"):
+        settings_file = open("settings.json")
+        settings = json.load(settings_file)
+        settings_file.close()
+        return settings
+
+if __name__ == '__main__':
+    # TODO: accept all settings as arguments
+    arguments = docopt(usage, version='Publish.py 0.1')
+    settings = SettingsLoader().load()
+
+    source_path = settings["source_path"]
+    build_path = settings["build_path"]
+    ignore = settings["ignore"]
+
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+    os.mkdir(build_path)
+
+    for (dirpath, dirnames, filenames) in os.walk(source_path):
+        reldirpath = dirpath.replace(source_path, "")
+        if reldirpath in ignore:
+            del dirnames[:]
+            del filenames[:]
+            continue
+
+        absdirpath = os.path.join(build_path, reldirpath)
+        if not os.path.exists(absdirpath):
+            os.mkdir(absdirpath)
+
+        # TODO: pull git submodules (which might bring in stuff like cv.tex)
+
+        for filename in filenames:
+            relfilepath = os.path.join(reldirpath, filename)
+            absfilepath = os.path.join(source_path, relfilepath)
+
+            if relfilepath in ignore:
+                continue
+
+            target = os.path.join(build_path, relfilepath)
+
+            if filename.endswith(".less"):
+                target = target.replace(".less", ".css")
+                # TODO: use python-less instead of invocating lessc
+                os.system("lessc -x {source} {destination}".format(source=absfilepath, destination=target))
+            # TODO: process HTML files, using jinja2
+            # TODO: process TEX files, converting them into PDFs
+            else:
+                shutil.copy(absfilepath, target)
+
+    if arguments["publish"]:
+        # TODO: search for a pure python rsync lib
+        rsync_command = "rsync -rtzchlC --delete-after {build_path} {target_path}"
+        rsync_command = rsync_command.format(build_path=build_path, target_path=settings["target_path"])
+        os.system(rsync_command)
