870dd764e7b9514d0a0e736fa72c24437e72f41b
Hugo Osvaldo Barrera <hugo@barrera.io>
2013-03-25 18:22:36 -0300

    Use jinja2 templating for html files.

--- publish.py
+++ publish.py
@@ -5,29 +5,60 @@ import sys
 import shutil
 import json
 from docopt import docopt
+from jinja2 import Environment, FileSystemLoader
 
 usage = """Publish.py
 
 Usage:
-  publish.py publish
-  publish.py build
+  publish.py publish [--settings=<FILE>]
+  publish.py build [--settings=<FILE>]
   publish.py -h | --help
   publish.py --version
 
+Options:
+  --settings=<FILE>    Use a different settings file.
 """
 
 class SettingsLoader:
 
-    def load(filename="settings.json"):
-        settings_file = open("settings.json")
+    @staticmethod
+    def load(settings_filename="settings.json"):
+        settings_file = open(settings_filename)
         settings = json.load(settings_file)
         settings_file.close()
         return settings
 
+class FileProcessor:
+
+    @staticmethod
+    def process_file(source_file, target_file):
+        cwd = os.path.dirname(source_file)
+        os.chdir(cwd)
+
+        if relfilepath.endswith(".less"):
+            target_file = target_file.replace(".less", ".css")
+            # TODO: use python-less instead of invocating lessc
+            os.system("lessc -x {source} {destination}".format(source=absfilepath, destination=target_file))
+        elif relfilepath.endswith(".html"):
+            env = Environment(loader=FileSystemLoader(cwd))
+            template = env.get_template(os.path.basename(source_file))
+            rendered_file = open(target_file, "w")
+            rendered_file.write(template.render())
+            rendered_file.close()
+        elif relfilepath.endswith(".tex"):
+            pass
+        # TODO: process TEX files, converting them into PDFs
+        else:
+            shutil.copy(absfilepath, target_file)
+
 if __name__ == '__main__':
     # TODO: accept all settings as arguments
     arguments = docopt(usage, version='Publish.py 0.1')
-    settings = SettingsLoader().load()
+
+    if arguments["--settings"]:
+        settings = SettingsLoader().load(arguments["--settings"])
+    else:
+        settings = SettingsLoader().load()
 
     source_path = settings["source_path"]
     build_path = settings["build_path"]
@@ -39,6 +70,8 @@ if __name__ == '__main__':
 
     for (dirpath, dirnames, filenames) in os.walk(source_path):
         reldirpath = dirpath.replace(source_path, "")
+
+        # Ignore all children if this directory is to be ignored:
         if reldirpath in ignore:
             del dirnames[:]
             del filenames[:]
@@ -59,14 +92,7 @@ if __name__ == '__main__':
 
             target = os.path.join(build_path, relfilepath)
 
-            if filename.endswith(".less"):
-                target = target.replace(".less", ".css")
-                # TODO: use python-less instead of invocating lessc
-                os.system("lessc -x {source} {destination}".format(source=absfilepath, destination=target))
-            # TODO: process HTML files, using jinja2
-            # TODO: process TEX files, converting them into PDFs
-            else:
-                shutil.copy(absfilepath, target)
+            FileProcessor().process_file(absfilepath, target)
 
     if arguments["publish"]:
         # TODO: search for a pure python rsync lib
