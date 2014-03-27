151bd0edadb2b6c45089b2be353ec0b486aa3de4
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-03-27 05:34:26 -0300

    Replaced publish.py with Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+  module.exports = function(grunt) {
+  defaultTasks = ['clean', 'jshint', 'less', 'copy', 'jinja']; // 'htmlmin'
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true,
+        },
+        ignores: "**/*min.js"
+      }
+    },
+    less: {
+      options: {
+          paths: ['src/assets/styles/*.less'],
+          report: 'gzip',
+          cleancss: true
+      },
+      src: {
+          expand: true,
+          cwd:    "src/assets/styles",
+          src:    "*.less",
+          dest:   "build/assets/styles",
+          ext:    ".css"
+      }
+    },
+    copy: {
+      files: {
+        expand: true,
+        cwd: 'src/',
+        src: ['**', '!**/*.less', '!**/*.html'],
+        dest: 'build/',
+        //ignores: ['**/*.js', '**/*.css'],
+        mode: 0644,
+      }
+    },
+    watch: {
+      default: {
+        files: ['src/**', 'Gruntfile.js'],
+        tasks: defaultTasks,
+        options: {
+          spawn: false,
+          atBegin: true
+        },
+      },
+    },
+    clean: ['build'],
+    // htmlmin: {
+    //   options: {
+    //     removeComments: true,
+    //     collapseWhitespace: true
+    //   },
+    //   files: {
+    //     expand: true,
+    //     cwd: 'src/',
+    //     src: ['**/*.html', '!**/*.jinja.html', '!**/_*.html'],
+    //     dest: 'build/'
+    //   }
+    // },
+    rsync: {
+      options: {
+        recursive: true
+      },
+      prod: {
+        options: {
+          src: "build/",
+          dest: "/var/www/https/hugo.barrera.io/",
+          host: "root@elysion.barrera.io",
+          syncDestIgnoreExcl: true
+        }
+      },
+    },
+    'http-server': {
+      local: {
+        root: 'build',
+
+        port: 8090,
+        host: "0.0.0.0",
+
+        cache: 0,
+        showDir: false,
+        autoIndex: true,
+        defaultExt: 'html',
+        runInBackground: false
+      }
+    },
+    jinja: {
+      default : {
+        options: {
+          templateDirs: ['src']
+        },
+        files: [{
+          expand: true,
+          dest: 'build/',
+          cwd: 'src/',
+          src: ['**/!(_)*.html']
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks("grunt-rsync");
+  grunt.loadNpmTasks("grunt-http-server");
+  grunt.loadNpmTasks("grunt-jinja");
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', defaultTasks);
+  grunt.registerTask('deploy', defaultTasks.concat(['rsync']));
+  grunt.registerTask('serve', defaultTasks.concat(['http-server']));
+};
--- README.md
+++ README.md
@@ -3,20 +3,9 @@ My Website
 
 This repository contains my personal website, https://hugo.barrera.io/.
 
-There's a small publication script included (which is still WIP), which pre-processes some file types before publishing.
-Hopefully, someone will find some use for this (rather simple) script.
-
-These are some features already implemented:
-
- * Process HTML file so as to use jinja2 templating.
- * Process [LESS](http://lesscss.org/) files into CSS.
- * Use rsync to publish changes.
-
-These are some pending features for when I get the time:
-
- * Pull submodules using git (for example, cv.pdf should be pulled from it's own repo).
- * Tidy up the code (this was a rather quick - and ugly - hack).
- * Process TEX files and just publish the resulting PDF files (but don't version them!).
+The code is fairly simple, and most of this repository is actual content
+(what little there is). There used to be a script that built/deployed/etc
+this website, but I've replaced it with [Grunt](http://gruntjs.com/).
 
 Licensing
 ---------
