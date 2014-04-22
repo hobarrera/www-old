a481a5a08acacf8a60bbaa5e70f8aab2468a00a5
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-04-22 13:12:45 -0300

    Converted recipes index page to markdown. Dynamically generate listing.

---
+++ src/recipes/index.js
@@ -0,0 +1,25 @@
+fs = require("fs");
+files = fs.readdirSync(__dirname).filter(function (filename) {
+  return filename.indexOf("_") !== 0 && filename.indexOf(".md") >= 0 &&
+    filename != "index.md";
+});
+
+recipes = [];
+files.forEach(function (file) {
+  var title = file;
+  while (title.indexOf("-") >= 0)
+    title = title.replace("-", " ");
+  title = title.replace(".md", " ");
+
+  var words = title.split(" ");
+  for (var i = 0; i < words.length; i++)
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+
+  var recipe = {};
+  recipe.title = words.join(' ');
+  recipe.href = file.replace(".md", ".html");
+
+  recipes.push(recipe);
+});
+
+module.exports.recipes = recipes;
---
+++ src/recipes/index.md
@@ -0,0 +1,8 @@
+Recipes
+=======
+
+Since I'm such a lover of sharing and free knowledge, I've put up this section with recipes for stuff I cook or cooked.  I also use it to quickly share a recipe when some acquaintance asks me how something is made, and, for course, for myself in the future.  
+All of these recipes are vegan-friendly.
+
+{% for recipe in recipes %} * [{{recipe.title}}](/recipes/{{recipe.href}})
+{% endfor %}
